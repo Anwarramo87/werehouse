f06e5e3246b9f4509837a7389e2d0396a68412a4
Anwar Ramo <anwarramo2333@gmail.com>
2026-04-05 21:39:27 +0300

    debug: add startup logs and global error handlers

--- backend-nest/src/main.ts
+++ backend-nest/src/main.ts
@@ -8,7 +8,21 @@ import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason) => {
+  console.error('Unhandled Rejection:', reason);
+  process.exit(1);
+});
+
 async function bootstrap() {
+  console.log('Bootstrap starting...');
+  console.log('NODE_ENV:', process.env.NODE_ENV);
+  console.log('PORT:', process.env.PORT);
+  console.log('REDIS_URL set:', !!process.env.REDIS_URL);
   const app = await NestFactory.create(AppModule);
   const config = app.get(ConfigService);
   const prisma = app.get(PrismaService);
