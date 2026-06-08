d4e0428e749c99265c998c0f889029ec63c976f6
Anwar Ramo <anwarramo2333@gmail.com>
2026-06-08 15:56:12 +0300

    ADS

--- backend-nest/src/auth/auth.service.ts
+++ backend-nest/src/auth/auth.service.ts
@@ -385,7 +385,7 @@ export class AuthService {
   private async handleAutoAttendance(user: any, dto: BiometricLoginFinishDto) {
     const employee = await this.prisma.employee.findFirst({
       where: {
-        OR: [{ contactEmail: user.email }, { employeeId: user.username.toUpperCase() }],
+        OR: [{ userId: user.id }, { employeeId: user.username.toUpperCase() }],
       },
     });
 
